ee4a0f67a663edfa0f103cb441b593e831a55a0c
Clemente Gomez <zomegpro@gmail.com>
2016-04-15 02:43:10 -0400

    Edit and Delete Products

--- models/product.js
+++ models/product.js
@@ -4,9 +4,25 @@ class Product{
 	constructor(){
 		this.sku = "";
 		this.name = "";
-		this.price = "";
-		this.quantity = "";
+		this._price = "";
+		this._quantity = "";
 		this.description = "";
+	}
+
+	set price(val){
+		this._price = Number(val);
+	}
+
+	get price(){
+		return this._price;
+	}
+
+	set quantity(val){
+		this._quantity = Number(val);
+	}
+
+	get quantity(){
+		return this._quantity;
 	}
 }
 
--- routes/products.js
+++ routes/products.js
@@ -16,14 +16,48 @@ class ProductsAPI{
 			}
 		})
 
+		_router.delete("/delete/:sku", (req, res) => {
+			console.log("Delete a Product");
+
+			_db.collection("products").findOneAndDelete({sku: req.sku})
+				.then((doc) => {
+					res.json({
+						message: "Product Deleted",
+						data: {
+							product: doc
+						}
+					})
+				}, (err) => {
+					console.error(err);
+					res.status(500).json({message: "Internal Error"});
+				})
+		})
+
+		_router.put("/edit/:sku", (req, res) => {
+			console.log("Edit a Product");
+
+			_db.collection("products").findOneAndUpdate({sku: req.sku}, {$set: req.body}, {returnOriginal: false})
+				.then((doc) => {
+					res.json({
+						message: "Product Updated",
+						data: {
+							product: doc
+						}
+					})
+				}, (err) => {
+					console.error(err);
+					res.status(500).json({message: "Internal Error"});
+				})
+		})
+
 		_router.post("/add", (req, res) => {
 			console.log("Add a product");
 
 			let product = new Product();
 			product.sku = req.body.sku;
 			product.name = req.body.name;
-			product.price = Number(req.body.price);
-			product.quantity = Number(req.body.quantity);
+			product.price = req.body.price;
+			product.quantity = req.body.quantity;
 			product.description = req.body.description;
 
 			_db.collection("products").find({sku: product.sku}).limit(1).next()
